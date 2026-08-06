4045cf07132ef2d40b41b5454191523712efdc04
Jorgee-hub <jmontiek217@gmail.com>
2026-08-06 03:48:54 +0000

    feat: models(en proceso aun)

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey, Date, Time, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import date, time
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= "user"
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password_hash= Mapped[str] = mapped_column(nullable=False)
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Destination(db.Model):
+    __tablename__= "destination"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    city: Mapped[str] = mapped_column(String(100), nullable=False)
+    country: Mapped[str] = mapped_column(String(100), nullable=False)
+
+
+class Trip(db.Model):
+    __tablename__= "trip"
+    id: Mapped[int] = mapped_column(primary_key="True")
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+class Activity(db.Model):
+    __tablename__= "activity"
+    id: Mapped[int] = mapped_column(primary_key="True")
+    time: Mapped[str] = mapped_column(date)
+    date: Mapped[str] = mapped_column(time)
+    notes: Mapped[str] = mapped_column(Text)
