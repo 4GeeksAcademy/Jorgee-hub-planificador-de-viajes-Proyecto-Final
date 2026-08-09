edd4c6bbfd0dccf183af4bd23d41d7c6e77a0c30
Jorgee-hub <jmontiek217@gmail.com>
2026-08-09 03:34:16 +0000

    feat: modelos base sin relaciones(User, Trip, Destination, Activity, Place, Favorite)

--- src/api/models.py
+++ src/api/models.py
@@ -1,32 +1,55 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, ForeignKey, Date, Time, Text
+from sqlalchemy import String, ForeignKey, Date, Time, Text, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from datetime import date, time
+from datetime import date, time, datetime
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    __tablename__= "user"
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-    password_hash= Mapped[str] = mapped_column(nullable=False)
-
-
-class Destination(db.Model):
-    __tablename__= "destination"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    city: Mapped[str] = mapped_column(String(100), nullable=False)
-    country: Mapped[str] = mapped_column(String(100), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(120))
+    last_name: Mapped[str] = mapped_column(String(120))
+    email: Mapped[str] = mapped_column(
+        String(120), nullable=False, unique=True)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
 
 
 class Trip(db.Model):
-    __tablename__= "trip"
-    id: Mapped[int] = mapped_column(primary_key="True")
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    __tablename__ = "trip"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    start_date: Mapped[date] = mapped_column(Date)
+    end_date: Mapped[date] = mapped_column(Date)
+
+
+class Destination(db.Model):
+    __tablename__ = "destination"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    city: Mapped[str] = mapped_column(String(120), nullable=False)
+    country: Mapped[str] = mapped_column(String(120), nullable=False)
+
 
 class Activity(db.Model):
-    __tablename__= "activity"
-    id: Mapped[int] = mapped_column(primary_key="True")
-    time: Mapped[str] = mapped_column(date)
-    date: Mapped[str] = mapped_column(time)
+    __tablename__ = "activity"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    time: Mapped[time] = mapped_column(Time)
+    date: Mapped[date] = mapped_column(Date)
     notes: Mapped[str] = mapped_column(Text)
+
+
+class Place(db.Model):
+    __tablename__ = "place"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    city: Mapped[str] = mapped_column(String(120), nullable=False)
+    country: Mapped[str] = mapped_column(String(120), nullable=False)
+    description: Mapped[str] = mapped_column(Text)
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorite"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.now())
